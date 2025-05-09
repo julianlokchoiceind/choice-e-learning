7a297e0489747fe2318ebeb0db9e31cd1755f2bc
julianlokchoiceind <julian.lok@choiceind.com>
2025-05-09 08:14:03 +0700

    20250508-5: Add PostCSS configuration and update Tailwind CSS settings
    - Created a new PostCSS configuration file to integrate Tailwind CSS and autoprefixer.
    - Updated Tailwind CSS configuration to streamline content paths and improve theme management.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    content: [
       './src/**/*.{js,ts,jsx,tsx,mdx}'  // Bắt tất cả files trong src
     ],
-  ],
   theme: {
     extend: {
       colors: {
